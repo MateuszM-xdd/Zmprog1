9a55aafe7336273a3916404df59473ee1254f6eb
MateuszM-xdd <mateusz.marsula@gmail.com>
2025-11-04 14:49:41 +0100

    Połączyć konflikt?

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
     cout<< "Hello, Git World" << endl;
-    cout<< "Laboratorium numer 1" << endl;
+    cout<< "Laboratorium numer 1" <<std::flush<<endl;
     return 0;
 }
